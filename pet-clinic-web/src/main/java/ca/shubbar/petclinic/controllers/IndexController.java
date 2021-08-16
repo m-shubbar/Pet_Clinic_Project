61076a2d996af483ce1b2e211a1660469beaa045
Mustafa <codingbox@outlook.com>
2021-08-16 19:13:42 -0400

    Index page and a controller added. closes #8.

---
+++ pet-clinic-web/src/main/java/ca/shubbar/petclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package ca.shubbar.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-16
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
