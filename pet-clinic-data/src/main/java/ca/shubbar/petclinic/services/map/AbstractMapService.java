2339b00787791a44caf2f67cf2f1a4fe11364e0a
Mustafa <codingbox@outlook.com>
2021-08-18 22:59:28 -0400

    Auto id setting implemented in the map. closes #23

--- pet-clinic-data/src/main/java/ca/shubbar/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/services/map/AbstractMapService.java
@@ -1,17 +1,16 @@
 package ca.shubbar.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import ca.shubbar.petclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * @author Mustafa <codingbox@outlook.com>
  * Created at 2021-08-16
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -22,8 +21,16 @@ public abstract class AbstractMapService<T, ID> {
     }
 
     // We will overwrite this because we don't know what will come here
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -35,6 +42,18 @@ public abstract class AbstractMapService<T, ID> {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
+
+    // implement id property setting
+    private Long getNextId(){
+
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch(NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
+    }
 
 
 }
--- pet-clinic-web/src/main/java/ca/shubbar/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ca/shubbar/petclinic/bootstrap/DataLoader.java
@@ -27,13 +27,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mustafa");
         owner1.setLastName("Sh.");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mike");
         owner2.setLastName("Ok.");
         ownerService.save(owner2);
@@ -41,13 +39,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Kathryn");
         vet1.setLastName("Ortega");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Elma");
         vet2.setLastName("Marjan");
         vetService.save(vet2);
