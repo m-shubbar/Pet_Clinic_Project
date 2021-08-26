8ba1fadb6c8b0b8d244d4c00c718340f8cb346d0
Mustafa <codingbox@outlook.com>
2021-08-26 16:56:31 -0400

    Vet JPA data model updated. closes #36.

--- pet-clinic-data/src/main/java/ca/shubbar/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/model/Speciality.java
@@ -1,11 +1,18 @@
 package ca.shubbar.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Mustafa <codingbox@outlook.com>
  * Created at 2021-08-23
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/ca/shubbar/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package ca.shubbar.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,8 +8,15 @@ import java.util.Set;
  * @author Mustafa <codingbox@outlook.com>
  * Created at 2021-08-14
  */
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    // JPA will try to load everything all in once
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
