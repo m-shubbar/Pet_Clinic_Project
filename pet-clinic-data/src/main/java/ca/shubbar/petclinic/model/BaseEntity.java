82742c2420c2f5e8b04d461ff052bb40a7133c3e
Mustafa <codingbox@outlook.com>
2021-08-26 12:56:57 -0400

    BaseEntity implemented. closes #34.

--- pet-clinic-data/src/main/java/ca/shubbar/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/model/BaseEntity.java
@@ -1,13 +1,22 @@
 package ca.shubbar.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author Mustafa <codingbox@outlook.com>
  * Created at 2021-08-15
  */
+
+// With this annotation, JPA will not map this class into the db
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     // Hibernate recommend the use of Boxed type, not the primitive type
     private Long id;
 
