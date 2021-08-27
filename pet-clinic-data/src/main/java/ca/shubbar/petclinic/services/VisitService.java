dc32fe0f34a97361037fa0657f5a96ac8993b24a
Mustafa <codingbox@outlook.com>
2021-08-26 22:13:16 -0400

    Visit mapping service added, closes #45.

---
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package ca.shubbar.petclinic.services;
+
+import ca.shubbar.petclinic.model.Visit;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-26
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package ca.shubbar.petclinic.services.map;
+
+import ca.shubbar.petclinic.model.Visit;
+import ca.shubbar.petclinic.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-26
+ */
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        // basic defensive validation to ensure that we have a properly formed object
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() ==null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
