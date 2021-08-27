cafeb82786ea4eca83725081942e07d91cc3e216
Mustafa <codingbox@outlook.com>
2021-08-26 21:38:48 -0400

    Pet JPA data service creacet, closes #42. Speciality JPA data service created, closes #43.

---
+++ pet-clinic-data/src/main/java/ca/shubbar/petclinic/services/datajpa/PetJPAService.java
@@ -0,0 +1,53 @@
+package ca.shubbar.petclinic.services.datajpa;
+
+import ca.shubbar.petclinic.model.Pet;
+import ca.shubbar.petclinic.repositories.PetRepository;
+import ca.shubbar.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-26
+ */
+
+@Service
+@Profile("jpadata")
+public class PetJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
