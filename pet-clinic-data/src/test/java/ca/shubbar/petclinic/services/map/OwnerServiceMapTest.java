b81989d1326cff027bb3821cfa0ecb59ae466759
Mustafa <codingbox@outlook.com>
2021-08-30 14:23:29 -0400

    OwnerServiceMap test added, closes #48

---
+++ pet-clinic-data/src/test/java/ca/shubbar/petclinic/services/map/OwnerServiceMapTest.java
@@ -0,0 +1,74 @@
+package ca.shubbar.petclinic.services.map;
+
+import ca.shubbar.petclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerServiceMapTest {
+
+    OwnerServiceMap ownerServiceMap;
+    final Long ownerId = 1L;
+    final String lastName = "Mustafa";
+
+    @BeforeEach
+    void setUp() {
+        ownerServiceMap = new OwnerServiceMap(new PetTypeServiceMap(), new PetServiceMap());
+        ownerServiceMap.save(Owner.builder().id(ownerId).lastName(lastName).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownersSet = ownerServiceMap.findAll();
+        assertEquals(1, ownersSet.size());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerServiceMap.findById(ownerId);
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        Owner owner2 = Owner.builder().id(id).build();
+        Owner savedOwner = ownerServiceMap.save(owner2);
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerServiceMap.save(Owner.builder().build());
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void delete() {
+        ownerServiceMap.delete(ownerServiceMap.findById(ownerId));
+        assertEquals(0, ownerServiceMap.findAll().size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerServiceMap.deleteById(ownerId);
+        assertEquals(0, ownerServiceMap.findAll().size());
+    }
+
+    @Test
+    void findByLastName() {
+        Owner mustafa = ownerServiceMap.findByLastName(lastName);
+        assertNotNull(mustafa);
+        assertEquals(ownerId, mustafa.getId());
+    }
+
+    @Test
+    void findByLastNameNotFound() {
+        Owner smith = ownerServiceMap.findByLastName("Jack");
+        assertNull(smith);
+    }
+}
--- pet-clinic-web/src/test/java/ca/shubbar/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/ca/shubbar/petclinic/PetClinicApplicationTests.java
@@ -1,10 +1,13 @@
 package ca.shubbar.petclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class PetClinicApplicationTests {
+public class PetClinicApplicationTests {
 
     @Test
     void contextLoads() {
